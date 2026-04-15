fe585d681c7765f02fed42c35c3f4045465acc30
Miroslav Jokovic <uncchicago85@gmail.com>
2026-04-15 15:03:25 -0500

    fix(csp): add TikTok and Meta pixel domains to script-src-elem and connect-src

--- augur/server/middleware/01.security.ts
+++ augur/server/middleware/01.security.ts
@@ -7,12 +7,12 @@ const MAX_BODY_BYTES = 512_000 // 512 KB
 
 const CSP = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' https://js.stripe.com https://static.cloudflareinsights.com",
-  "script-src-elem 'self' 'unsafe-inline' https://js.stripe.com https://static.cloudflareinsights.com",
+  "script-src 'self' 'unsafe-inline' https://js.stripe.com https://static.cloudflareinsights.com https://analytics.tiktok.com https://connect.facebook.net",
+  "script-src-elem 'self' 'unsafe-inline' https://js.stripe.com https://static.cloudflareinsights.com https://analytics.tiktok.com https://connect.facebook.net",
   "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
   "font-src 'self' https://fonts.gstatic.com",
   "img-src 'self' data: blob: https:",
-  "connect-src 'self' https://api.stripe.com https://checkout.stripe.com https://cloudflareinsights.com",
+  "connect-src 'self' https://api.stripe.com https://checkout.stripe.com https://cloudflareinsights.com https://analytics.tiktok.com https://connect.facebook.net https://www.facebook.com",
   "frame-src https://js.stripe.com https://hooks.stripe.com",
   "object-src 'none'",
   "base-uri 'self'",
