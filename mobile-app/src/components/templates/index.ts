32b4b9a3d844505afaccf015aad113c8db99c2b5
Miroslav Jokovic <uncchicago85@gmail.com>
2026-05-07 11:39:04 -0500

    [DS] templates: ScreenWrapper, OnboardingStep, PaywallShell, EmptyState, ErrorState

---
+++ mobile-app/src/components/templates/index.ts
@@ -0,0 +1,14 @@
+export { ScreenWrapper } from './ScreenWrapper'
+export type { ScreenWrapperProps } from './ScreenWrapper'
+
+export { OnboardingStep } from './OnboardingStep'
+export type { OnboardingStepProps } from './OnboardingStep'
+
+export { PaywallShell } from './PaywallShell'
+export type { PaywallShellProps } from './PaywallShell'
+
+export { EmptyState } from './EmptyState'
+export type { EmptyStateProps } from './EmptyState'
+
+export { ErrorState } from './ErrorState'
+export type { ErrorStateProps } from './ErrorState'
